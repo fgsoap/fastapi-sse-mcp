dc2a42fabdd874201797e84a059cc48fbe8cf33f
Jian Xu <jianxu1@microsoft.com>
2025-04-21 14:14:47 +0800

    update

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 logger = logging.getLogger(__name__)
 
 # Replace with your Flask app URL
-url = "https://fastapi-sse-mcp.onrender.com"
+url = "https://www.google.com/"
 
 
 def reload_website():
